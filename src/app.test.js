6ae25859b9d2faa1960065b14b56109ceb584838
rahul <tkdmaster.sharma@gmail.com>
2018-06-20 15:12:37 -0700

    initial commit

--- src/app.test.js
+++ src/app.test.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import Order from './components/order';
 import Shop from './components/shop';
 import App from './App';
 
 describe('App component', () => {
+  let shallowWrapper;
+  beforeEach(() => {
+    shallowWrapper = shallow(<App />);
+  });
   describe('App component', () => {
-    const shallowWrapper = shallow(<App/>);
-
     test('renders correctly', () => {
       expect(shallowWrapper).toMatchSnapshot();
     });
 
     test('has "Order" component', () => {
-      expect(shallowWrapper.containsMatchingElement(<Order/>)).toEqual(true);
+      expect(shallowWrapper.containsMatchingElement(<Order />)).toEqual(true);
     });
 
     test('has "Shop" component', () => {
-      expect(shallowWrapper.containsMatchingElement(<Shop/>)).toEqual(true);
+      expect(shallowWrapper.containsMatchingElement(<Shop />)).toEqual(true);
     });
-
   });
 });
